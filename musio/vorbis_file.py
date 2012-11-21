22d998c0e63a0400e710b3b1b64d29eb7ea91e5f
Josiah Gordon <josiahg@gmail.com>
2012-11-21 13:24:29 -0600

    Handle invalid comments

--- musio/vorbis_file.py
+++ musio/vorbis_file.py
@@ -181,6 +181,11 @@ class VorbisFile(AudioIO):
             if not comment: break
 
             comment_list = comment.decode('utf8', 'replace').split('=')
+
+            # Handle invalid comments (without '=')
+            if '=' not in comment:
+                comment_list.insert(0, 'misc')
+
             comment_list[0] = comment_list[0].lower()
 
             self._info_dict.update(dict((comment_list,)))
