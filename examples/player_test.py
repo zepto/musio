e54eb2d4d9f37d1e7317ef2511d9d6650b1c9cde
Josiah Gordon <josiahg@gmail.com>
2012-12-22 08:21:15 -0600

    Setup VTIME and VMIN

--- examples/player_test.py
+++ examples/player_test.py
@@ -32,7 +32,8 @@ def main(args: dict) -> None:
     from sys import stdin as sys_stdin
     from select import select
     from time import sleep as time_sleep
-    from termios import tcgetattr, tcsetattr, ECHO, ICANON, TCSANOW
+    from termios import tcgetattr, tcsetattr
+    from termios import ECHO, ICANON, TCSANOW, VTIME, VMIN
 
     from musio.player_util import AudioPlayer
 
@@ -55,6 +56,8 @@ def main(args: dict) -> None:
 
     # Do not wait for key press and don't echo.
     quiet[3] &= ~(ECHO | ICANON)
+    quiet[6][VTIME] = 0
+    quiet[6][VMIN] = 0
 
     # Set the new terminal state.
     tcsetattr(sys_stdin, TCSANOW, quiet)
